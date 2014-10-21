d12ee5de983990168fec4aa7382780fa131de2e0
Jeremy Cohen Solal(jcohen) <jeremy.cohen-solal@rentabiliweb.com>
2014-10-21 11:41:32 +0200

    Add some batch example Add a bin script for batch

---
+++ src/Be2bill/Api/Batch/Observer/Sleep.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class Be2bill_Api_Batch_Observer_Sleep
+ * Use it for configuring some sleep time between each transactions
+ */
+class Be2bill_Api_Batch_Observer_Sleep implements SplObserver
+{
+    protected $sleep;
+
+    /**
+     * @param $msec Miliseconds
+     */
+    public function __construct($msec)
+    {
+        $this->sleep = $msec;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Receive update from subject
+     * @link http://php.net/manual/en/splobserver.update.php
+     * @param SplSubject $subject <p>
+     * The <b>SplSubject</b> notifying the observer of an update.
+     * </p>
+     * @return void
+     */
+    public function update(SplSubject $subject)
+    {
+        usleep($this->sleep * 1000);
+    }
+}
